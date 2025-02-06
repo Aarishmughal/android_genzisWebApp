cf482deae13f2cce3a24f8aa18fac6a8d9b1f90f
Aarishmughal <aarishmughal21@gmail.com>
2025-02-07 02:26:52 +0500

    Initial Commit

---
+++ app/src/main/java/com/faisal/giftngears/thelegacy/MainActivity.java
@@ -0,0 +1,67 @@
+package com.faisal.giftngears.thelegacy;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout mainLayout = findViewById(R.id.main);
+
+        // Apply a listener to adjust padding based on WindowInsets
+        mainLayout.setOnApplyWindowInsetsListener((view, insets) -> {
+            // Adjust the padding of the top of the mainLayout
+            int statusBarHeight = 0;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                statusBarHeight = insets.getSystemGestureInsets().top;
+            }
+            view.setPadding(0, statusBarHeight, 0, 0);
+            return insets;
+        });
+
+        WebView webView = findViewById(R.id.webview);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        webView.loadUrl("https://faisal.giftngears.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = findViewById(R.id.webview);
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
